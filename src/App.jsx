24cf67220787a1e67ee355fdd71222df69b12719
ge-arty <george.arutinyan@gmail.com>
2023-05-15 15:53:34 +0400

    projectUpload

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import moon from "./assets/moon.png";
+import cat from "./assets/cat.gif";
+import land from "./assets/land.png";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useRef } from "react";
+
+function App() {
+  const ref = useRef();
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: "cover",
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: "center" }}
+        >
+          <img src={cat} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>To the Land</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>To the Moon</h2>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
